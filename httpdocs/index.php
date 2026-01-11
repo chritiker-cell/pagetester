824b5ec61ca056ba66dd7d3b6bba285f1ee23fff
chritiker-cell <chri.marak@gmail.com>
2026-01-11 17:02:24 +0100

    Initial commit – komplette Website vom FTP importiert

---
+++ httpdocs/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>phantom-solutions</title>
+    <link rel="stylesheet" href="styles.css">
+    <script defer src="script.js"></script>
+</head>
+<body background-color:=yellow>
+    <header>       
+    </header>
+    <main>
+	    <center> 
+			<section>
+				</br> <b></b> </br>
+				</br> 
+				<h2><b>itcoach-wehofer</b></h2>
+				<p>Herzlich Willkommen im sonnigen Freiburg im Braisgau<br>
+				   hier entsteht in kürze mein Angebot, derzeit nur als Test OHNE Angebote und OHNE Relevanz 
+				</p>
+			</section>					
+		</center>
+    </main>
+    <footer>
+        <div class="container">
+            <p>&copy; 2024 itcoach-wehofer. Alle Rechte vorbehalten.</p>
+        </div>
+    </footer>
+</body>
+</html>
